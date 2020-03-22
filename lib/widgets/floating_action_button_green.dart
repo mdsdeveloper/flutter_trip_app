4b2bd30b735fc3636484e87a555beb887ddeeda9
Guillermina Mª <mdsmatias@hotmail.com>
2020-03-22 18:27:24 +0100

    Segundo commit

---
+++ lib/widgets/floating_action_button_green.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class FloatingActionButtonGreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _FloatingActionButtonGreen();
+  }
+
+}
+
+
+class _FloatingActionButtonGreen extends State<FloatingActionButtonGreen> {
+  bool pressedButton = false;
+
+  void onPressedFav() {
+    setState(() {
+      pressedButton = !pressedButton;
+    });
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: pressedButton
+          ? Text("Agregaste a tus favoritos")
+          : Text("Quitaste de tus favoritos"),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      backgroundColor: Color(0xFF11DA53),
+      mini: true,
+      tooltip: "Fav",
+      onPressed: onPressedFav,
+      child: Icon(pressedButton ? Icons.favorite : Icons.favorite_border),
+    );
+  }
+}
