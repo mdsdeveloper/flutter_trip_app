4b4f6d765e5d6e2f0123b17c6d11fb38f9cb5b4e
Guillermina Mª <mdsmatias@hotmail.com>
2020-03-28 19:29:06 +0100

    Añado logout en la app

--- lib/User/ui/widgets/button_bar.dart
+++ lib/User/ui/widgets/button_bar.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertripapp/User/bloc/bloc_user.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'circle_button.dart';
 
 class ButtonsBar extends StatelessWidget {
+  BlocUser blocUser;
 
   @override
   Widget build(BuildContext context) {
+    blocUser = BlocProvider.of(context);
     return Padding(
-        padding: EdgeInsets.symmetric(
-            horizontal: 0.0,
-            vertical: 10.0
-        ),
+        padding: EdgeInsets.symmetric(horizontal: 0.0, vertical: 10.0),
         child: Row(
           children: <Widget>[
 //            CircleButton(true, Icons.turned_in_not, 20.0, Color.fromRGBO(255, 255, 255, 1)),
-            CircleButton(true, Icons.vpn_key, 20.0, Color.fromRGBO(255, 255, 255, 0.6)),
-            CircleButton(false, Icons.add, 40.0, Color.fromRGBO(255, 255, 255, 1)),
-            CircleButton(true, Icons.exit_to_app, 20.0, Color.fromRGBO(255, 255, 255, 0.6))
+            CircleButton(true, Icons.vpn_key, 20.0,Color.fromRGBO(255, 255, 255, 0.6), () => {}),
+            CircleButton(false, Icons.add, 40.0,Color.fromRGBO(255, 255, 255, 1), () => {}),
+//            Cierra sesion
+            CircleButton(
+                true,
+                Icons.exit_to_app,
+                20.0,
+                Color.fromRGBO(255, 255, 255, 0.6),
+                () => {blocUser.signOuth()}),
 //            CircleButton(true, Icons.person, 20.0, Color.fromRGBO(255, 255, 255, 0.6))
           ],
-        )
-    );
+        ));
   }
-
 }
