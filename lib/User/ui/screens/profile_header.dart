f568107654765210eebe55f1ecddcc92425b49a5
Guillermina Mª <mdsmatias@hotmail.com>
2020-03-29 20:50:18 +0200

    Añado lógica para comportamiento si está logado el usuario o no, se redirige a login o a home

--- lib/User/ui/screens/profile_header.dart
+++ lib/User/ui/screens/profile_header.dart
@@ -1,39 +1,74 @@
 import 'package:flutter/material.dart';
-import '../widgets/user_info.dart';
-import '../widgets/button_bar.dart';
+import 'package:fluttertripapp/User/bloc/bloc_user.dart';
+import 'package:fluttertripapp/User/model/user.dart';
+import 'package:fluttertripapp/User/ui/widgets/button_bar.dart';
+import 'package:fluttertripapp/User/ui/widgets/user_info.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 
 class ProfileHeader extends StatelessWidget {
+  BlocUser blocUser;
+  User user;
+
   @override
   Widget build(BuildContext context) {
-
-    final title = Text(
-      'Profile',
-      style: TextStyle(
-          fontFamily: 'Lato',
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: 30.0
-      ),
-    );
-
-    return Container(
-      margin: EdgeInsets.only(
-          left: 20.0,
-          right: 20.0,
-          top: 50.0
-      ),
-      child: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              title
-            ],
-          ),
-          UserInfo('assets/img/ann.jpg', 'Anahí Salgado','anahi@platzi.com'),
-          ButtonsBar()
-        ],
-      ),
+    blocUser = BlocProvider.of(context);
+    return StreamBuilder(
+      stream: blocUser.streamFirebase,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return CircularProgressIndicator();
+          case ConnectionState.none:
+            return CircularProgressIndicator();
+          case ConnectionState.active:
+            return showProfileData(snapshot);
+          case ConnectionState.done:
+            return showProfileData(snapshot);
+        }
+      },
     );
   }
 
+  Widget showProfileData(AsyncSnapshot snapShot) {
+    if (!snapShot.hasData || snapShot.hasError) {
+      print("No legeado");
+      return Container(
+        margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 50.0),
+        child: Column(
+          children: <Widget>[
+            CircularProgressIndicator(),
+            Text("No se pudo carcar la información. Has login"),
+          ],
+        ),
+      );
+    } else {
+      print("Logeado");
+      print(snapShot.data);
+      user = User(
+          name: snapShot.data.displayName,
+          email: snapShot.data.email,
+          photoUrl: snapShot.data.photoUrl);
+      final title = Text(
+        'Profile',
+        style: TextStyle(
+            fontFamily: 'Lato',
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 30.0),
+      );
+
+      return Container(
+        margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 50.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[title],
+            ),
+            UserInfo(user),
+            ButtonsBar()
+          ],
+        ),
+      );
+    }
+  }
 }
